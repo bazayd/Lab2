537d3f6aa865b3dee376f64e9661f506c8d674db
Brandon Arriaga <brandonzarriaga@gmail.com>
2023-07-13 15:12:26 -0700

    First Commit.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,38 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     abstract static class Currency {
+        private int whole;
+        private int fractional;
+
+        public int add() {
+            return 1;
+        }
+
+        public int substract() {
+            return 1;
+        }
+
+        public boolean isEqual() {
+            return false;
+        }
+
+        public boolean isGreat() {
+            return false;
+        }
+
+        public String toString() {
+            return " ";
+        }
 
     }
 
     static class Dollar extends Currency {
+        public String currencyName;
 
     }
 
     static class Pound extends Currency {
+        public String currencyName;
 
     }
 
