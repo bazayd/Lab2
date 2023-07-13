738f7e7cb829b88c70305036e777b594805799e5
Brandon Arriaga <brandonzarriaga@gmail.com>
2023-07-13 16:12:22 -0700

    Added necessary files

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,5 @@
 
 public class Main {
-    // Abstract base class
-    abstract static class Currency {
-        private int whole;
-        private int fractional;
-
-        public int add() {
-            return 1;
-        }
-
-        public int substract() {
-            return 1;
-        }
-
-        public boolean isEqual() {
-            return false;
-        }
-
-        public boolean isGreat() {
-            return false;
-        }
-
-        public String toString() {
-            return " ";
-        }
-
-    }
-
-    abstract static class Dollar extends Currency {
-        public String currencyName;
-
-    }
-
-    abstract static class Pound extends Currency {
-        public String currencyName;
-
-    }
-
-
 
     public static void main(String[] args) {
 
