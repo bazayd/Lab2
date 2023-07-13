c265b4b8c667067f84d74b896c9698e7185a9880
Brandon Arriaga <brandonzarriaga@gmail.com>
2023-07-13 15:06:08 -0700

    Initial Commit.

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    abstract static class Currency {
+
+    }
+
+    static class Dollar extends Currency {
+
+    }
+
+    static class Pound extends Currency {
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
