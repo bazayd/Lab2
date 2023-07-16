a470ee5746e8e9fb375002049015a92b128d6e1b
Nick42-beep <nichlekang@gmail.com>
2023-07-16 15:46:57 -0700

    handling input in main

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,50 @@ public class Main {
             System.out.println(currency);
         }
 
+        /*
+         * while (input not q)
+         * get 2nd letter
+         * then get the number
+         * if p or d add to dollar or pound
+         */
 
+        String op = ""; // operation (add a / subtract s)
+        String cur; //currency type (pound p / dollar d)
+        double value; //value of currency (double)
+        
+        while(op != "q") {
+        	
+        	System.out.println("Enter pound or dollar object: ");
+        	
+        	op = scan.next();
+            cur = scan.next();
+            value = scan.nextDouble();
+            
+            System.out.println(op + " " + cur + " " + value);
+            
+            if (op.equals("a")) {
 
+        		if (cur.equals("p")) {
+        			currencies[0].add(new Pound(value));
+        			//System.out.println(currencies[0]);
+        			System.out.println(currencies[0].getWhole());
+        			System.out.println(currencies[0].getFraction());
+        			System.out.println(currencies[0].toString());
+;        		}else {
+        			currencies[1].add(new Dollar(value));
+        		}
+        	}else if (op.equals("s")) {
+        		if (cur.equals("p")) {
+        			currencies[0].subtract(new Pound(value));
+        		}else {
+        			currencies[1].subtract(new Dollar(value));
+        		}
+        	}
+//            System.out.println("Currencies in use: ");
+//            for (Currency currency : currencies) {
+//                System.out.println(currency);
+//            }
+            
+        }
     }
 }
